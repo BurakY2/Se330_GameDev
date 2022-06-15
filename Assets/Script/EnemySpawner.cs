c6e2242bc83b57968b8ecfe42f450f4a4c963fe6
AtakanAkyaz <atakanakyaz@hotmail.com>
2022-06-15 14:40:08 -0700

    15.06.2022

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public int WaveCounter = 5;
+    private float WaveTime = 0f;
+    //public Transform SpawnPoint;
+    public GameObject[] SpawnPoints;
+    public GameObject SpawnPoint;
+    public GameObject Enemy;
+
+
+
+    private void Start()
+    {
+        SpawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    }
+
+    private void Update()
+    {
+        if (WaveTime >= 5f && WaveCounter != 0)
+        {
+            StartCoroutine(SpawnEnemies());
+            WaveTime = 0f;
+            WaveCounter--;
+        }
+
+        WaveTime += Time.deltaTime;
+
+
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        if (WaveCounter == 5)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(2f);
+
+            }
+        }
+        else if (WaveCounter == 4)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        else if (WaveCounter == 3)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        else if (WaveCounter == 2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+
+        }
+        else if (WaveCounter == 1)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+
+
+        }
+
+    }
+
+
+    void SpawnEnemy()
+    {
+        Transform SpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform;
+
+        Instantiate(Enemy, SpawnPoint.position, Quaternion.Euler(0, 0, 0));
+    }
+}
