8a737761e0c66625d3d6108a4ab0f3a5009d0676
Burak Yalçın <yalcnburakk@gmail.com>
2022-06-27 16:26:59 +0300

    aa

--- Assets/Script/Enemy Range/Enemyranger.cs
+++ Assets/Script/Enemy Range/Enemyranger.cs
@@ -1,18 +1,62 @@
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemyranger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform goal;
+    private Animator anim;
+    private NavMeshAgent agent;
+    public bool AtackingTrigger = false;
+
     void Start()
     {
-        
+        anim = gameObject.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.LogWarning(agent.stoppingDistance);
+        Debug.LogWarning("Remaining distance: " + agent.remainingDistance);
+        try
+        {
+            float a = agent.speed;
+            anim.SetFloat("Running", a);
+            agent.destination = goal.position;
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                anim.SetFloat("Running", 0);
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                Debug.Log("Attacking");
+                anim.SetTrigger("Attack");
+            }
+
+        }
+        catch
+        {
+            agent.speed = 0;
+            Debug.Log("öldün çık");
+            agent.isStopped = true;
+
+        }
+
+
+
+
+
+
     }
-}
+
+
+
+}  
+    
