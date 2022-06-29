3bcdb5288250a4e9b2bdf4b35b39f421a4f1c111
AtakanAkyaz <61373558+AtakanAkyaz@users.noreply.github.com>
2022-06-29 17:12:00 +0300

    29.06

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -10,6 +10,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject[] SpawnPoints;
     public GameObject SpawnPoint;
     public GameObject Enemy;
+    public GameObject RangedEnemy;
 
 
 
@@ -79,6 +80,7 @@ public class EnemySpawner : MonoBehaviour
             for (int i = 0; i < 5; i++)
             {
                 SpawnEnemy();
+                SpawnRangedEnemy();
                 yield return new WaitForSeconds(1f);
             }
             for (int i = 0; i < 5; i++)
@@ -93,6 +95,7 @@ public class EnemySpawner : MonoBehaviour
             for (int i = 0; i < 7; i++)
             {
                 SpawnEnemy();
+                SpawnRangedEnemy();
                 yield return new WaitForSeconds(1f);
             }
 
@@ -107,5 +110,12 @@ public class EnemySpawner : MonoBehaviour
         Transform SpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform;
 
         Instantiate(Enemy, SpawnPoint.position, Quaternion.Euler(0, 0, 0));
+    }
+
+    void SpawnRangedEnemy()
+    {
+        Transform SpawnPoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform;
+
+        Instantiate(RangedEnemy, SpawnPoint.position, Quaternion.Euler(0, 0, 0));
     }
 }
